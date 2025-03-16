c0f0a80ad8ab6fed6cd960c1bb1baa3fb4c36232
hujunnan <hujunnan@aisthink.com>
2025-03-16 11:46:33 +0800

    targetSemCount 2000

--- erigon-lib/kv/remotedb/kv_remote.go
+++ erigon-lib/kv/remotedb/kv_remote.go
@@ -99,7 +99,7 @@ func (opts remoteOpts) Open() (*DB, error) {
 		targetSemCount = 2
 	}
 
-	targetSemCount = 10000
+	targetSemCount = 2000
 	fmt.Println("targetSemCount", targetSemCount)
 	fmt.Println("opts", opts)
 
