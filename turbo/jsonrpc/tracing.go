08c39011442af765df92c6a47a852d29543e5f61
hujunnan <hujunnan@aisthink.com>
2025-05-31 18:24:37 +0800

    1. add blockoverrides for debug_traceCall

--- turbo/jsonrpc/tracing.go
+++ turbo/jsonrpc/tracing.go
@@ -426,6 +426,9 @@ func (api *PrivateDebugAPIImpl) TraceCall(ctx context.Context, args ethapi.CallA
 
 	blockCtx := transactions.NewEVMBlockContext(engine, header, blockNrOrHash.RequireCanonical, dbtx, api._blockReader, chainConfig)
 	txCtx := core.NewEVMTxContext(msg)
+
+	blockHeaderOverride(&blockCtx, config.BlockOverrides, nil)
+
 	// Trace the transaction and return
 	_, err = transactions.TraceTx(ctx, engine, msg, blockCtx, txCtx, hash, 0, ibs, config, chainConfig, stream, api.evmCallTimeout)
 	return err
