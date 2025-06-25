5529219babfb9a00eda61c4cd39a1a7a55197843
hujunnan <hujunnan@aisthink.com>
2025-06-25 17:49:15 +0800

    1.  fix stateOverrides in traceCallMany

--- turbo/jsonrpc/tracing.go
+++ turbo/jsonrpc/tracing.go
@@ -555,7 +555,7 @@ func (api *PrivateDebugAPIImpl) TraceCallMany(ctx context.Context, bundles []Bun
 		stream.WriteArrayStart()
 		// first change blockContext
 		blockHeaderOverride(&blockCtx, bundle.BlockOverride, overrideBlockHash)
-		ibs.Reset()
+		// ibs.Reset()
 		for txnIndex, txn := range bundle.Transactions {
 			if txn.Gas == nil || *(txn.Gas) == 0 {
 				txn.Gas = (*hexutil.Uint64)(&api.GasCap)
